cd0ec0f5aab60417f17236d6f6368f090841af04
liujiahui3 <1609307967@qq.com>
2020-03-28 14:40:31 +0800

    page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import {Route,HashRouter,Redirect} from 'react-router-dom'
 import Box from '../pages/Box'
 import Login from '../pages/Login'
 import Root from '../pages/Root'
+import GoodsList from '../pages/Goods/goodslist'
+import Goodsupdate from '../pages/Goods/goodsupdate'
+import Goodsadd from '../pages/Goods/goodsadd'
+import User from '../pages/User'
 class Router extends Component {
   state = {  }
   render() { 
@@ -14,6 +18,10 @@ class Router extends Component {
             <Box>
               {/* box组件内容区显示的组件,通过props.children传递 */}
                <Route path="/box/root" component={Root}></Route>
+               <Route path="/box/user" component={User}></Route>
+               <Route path="/box/goodslist" component={GoodsList}></Route>
+               <Route path="/box/goodsupdate" component={Goodsupdate}></Route>
+               <Route path="/box/goodsadd" component={Goodsadd}></Route>
             </Box>
           )
         }}>
@@ -24,5 +32,5 @@ class Router extends Component {
      );
   }
 }
- 
+
 export default Router;
