f46afdc4bb39c89d7e7b0109b01db46a9e3adf2b
liujiahui3 <1609307967@qq.com>
2020-03-29 14:24:25 +0800

    分类列表的渲染

--- src/pages/Classify/sort.js
+++ src/pages/Classify/sort.js
@@ -1,42 +1,77 @@
 import React, { Component } from 'react'
 import style from './index.moudle.less'
 import { Collapse, Card,Button } from 'antd';
+const {Panel} = Collapse
+    class Sort extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            listData: [
+                {
+                    header:"推荐专区",
+                    key:"1",
+                    childer:[
+                        {
+                            header:"充值特惠",
+                            key:"1-1"
+                        },
+                        {
+                            header:"口碑好物",
+                            key:"1-2"
+                        },
+                        {
+                            header:"快手新菜",
+                            key:"1-3"
+                        }
+                    ]
+                },
+                {
+                    header:"宅家防护",
+                    key:"2",
+                    childer:[
+                        {
+                            header:"消毒杀菌",
+                            key:"2-1"
+                        },
+                        {
+                            header:"换季单品",
+                            key:"2-2"
+                        },
+                        {
+                            header:"滋补美食",
+                            key:"2-3"
+                        }
+                    ]
+                }
 
-class Sort extends Component {
-
+            ]
+        }
+    }
+    listRender=(list)=>{
+        return list.map(item=>{
+            if(item.childer){
+                return (
+                    <Panel header={item.header} key={item.key}>
+                        <Collapse defaultActiveKey={item.key}>
+                            {this.listRender(item.childer)}
+                        </Collapse>
+                    </Panel>
+                )
+            }else{
+                return (
+                    <Panel header={item.header} key={item.key}> 
+                        <Button type="primary">点击管理</Button>
+                    </Panel>
+                )
+            }
+        })
+    }
     render() {
-        let {Panel}=Collapse
         return (
             <div className={style.box}>
                 <Card title="商品分类管理" className={style.card}>
                     <Collapse>
-                        <Panel header="推荐专区" key="1">
-                            <Collapse defaultActiveKey="1">
-                               
-                                <Panel header="充值特惠" key="1-1"> 
-                                  <Button type="primary">点击管理</Button>
-                                </Panel>
-                                <Panel header="口碑好物" key="1-2">
-                                    <Button type="primary">点击管理</Button>
-                                </Panel>
-                                <Panel header="快手新菜" key="1-3">
-                                  <Button type="primary">点击管理</Button>
-                                </Panel>
-                            </Collapse>
-                        </Panel>
-                        <Panel header="宅家防护" key="2">
-                               <Collapse defaultActiveKey="2"> 
-                                <Panel header="消毒杀菌" key="2-1">
-                                  <Button type="primary">点击管理</Button>
-                                </Panel>
-                                <Panel header="换季单品" key="2-2">
-                                    <Button type="primary">点击管理</Button>
-                                </Panel>
-                                <Panel header="滋补美食" key="2-3">
-                                  <Button type="primary">点击管理</Button>
-                                </Panel>
-                            </Collapse>
-                        </Panel>
+                        {this.listRender(this.state.listData)}
                         <Panel header="爆品专区" key="3">
                                 <Collapse defaultActiveKey="3">
                                 <Panel header="居家生活" key="3-1">
