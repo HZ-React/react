3406a356916c271f62742d178ca0a702b6c21286
mading <849793164@qq.com>
2020-03-30 11:26:39 +0800

    dd

--- src/api/user.js
+++ src/api/user.js
@@ -1,15 +1,19 @@
 import axios from '../middleware/axios'
 class User {
-    userlist(token){
-      let url = `/mall/user/list?token=${localStorage.getItem('userToken')}`
-      return axios.get(url,token)
-    }
-    useradd(us,ps){
+    // userlist(token){//列表渲染
+    //   let url = `/mall/user/list?token=${localStorage.getItem('userToken')}`
+    //   return axios.get(url,token)
+    // }
+    userlist(token,page = 1,pageSize =2){//列表渲染
+        let url = `/mall/user/list?token=${localStorage.getItem('userToken')}`
+        return axios.get(url,token,{params:{page,pageSize}})
+      }
+    useradd(us,ps){//增加
         let url = '/mall/user/add'
         let token = localStorage.getItem('userToken')
         return axios.post(url,{us,ps,token})
       }
-    userdel(_id){
+    userdel(_id){//删除
         let token = localStorage.getItem('userToken')
         let url = `/mall/user/del?token=${token}&_id=${_id}`
         return axios.get(url)
